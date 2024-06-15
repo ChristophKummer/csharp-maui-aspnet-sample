bda39d089d2919590e5aa570d3e4f87814f59e54
Christoph Kummer <christophkummer7@gmail.com>
2024-06-15 16:33:34 +0200

    update

---
+++ Sources/CustomApp/Models/Pages/CardView.cs
@@ -0,0 +1,15 @@
+namespace CustomApp.Models.Pages;
+
+public class CardView : ContentView
+{
+	public CardView()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
